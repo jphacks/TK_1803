00c07ea849f6efa2baf3b04667975b3f917b669f
Ayato Tokubi <tokubi.ayato>
2018-10-28 00:51:09 +0900

    delayを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_27_053828) do
+ActiveRecord::Schema.define(version: 2018_10_27_142117) do
 
   create_table "session_players", force: :cascade do |t|
     t.integer "user_id"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2018_10_27_053828) do
     t.string "instrument"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "delay"
     t.index ["user_id"], name: "index_session_players_on_user_id"
     t.index ["web_session_id"], name: "index_session_players_on_web_session_id"
   end
