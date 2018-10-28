8283a02d081b3a04b4fd31f68b150f462ed8ae2c
Mai1126 <mai.11260923@gmail.com>
2018-10-28 14:23:34 +0900

    master rails | EDIT seedsの投入

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,6 +3,5 @@ class Video < ApplicationRecord
   validates :name, {presence: true}
   validates :user, {presence: true}
   validates :instrument, {presence: true}
-  validates :url, {presence: true, uniqueness: true}
-  validates :score, {presence: true}
+  validates :url, {presence: true}
 end
